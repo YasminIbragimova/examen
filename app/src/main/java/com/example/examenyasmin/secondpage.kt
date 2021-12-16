01d1296e22448d07825b94efd709689ed1403548
Admin <ibragimova_y@auca.kg>
2021-12-16 15:07:18 +0600

    yasmin2

--- app/src/main/java/com/example/examenyasmin/secondpage.kt
+++ app/src/main/java/com/example/examenyasmin/secondpage.kt
@@ -1,11 +1,18 @@
 package com.example.examenyasmin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class secondpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.secondpage)
+        val textView = findViewById<Button>(R.id.button2)
+
+        textView.setOnClickListener{
+            startActivity(Intent(this,MainPage::class.java))
+        }
     }
 }
